b8a29d41ddab9311079f5fc630bcabe571ffecf0
RileyManda <rileymanda69@gmail.com>
2023-07-04 22:44:45 +0200

    Code cleanup-console log statements

--- src/utils/recordLikes.js
+++ src/utils/recordLikes.js
@@ -4,27 +4,22 @@ import showSnackbar from './showSnackBar.js';
 const recordLikes = (itemId) => {
   const url = `${ENV_API}${APP_ID}/likes/`;
 
-  console.log('itemId:', itemId);
-
   fetch(url, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ item_id: itemId }), // Use "item_id" as the key
+    body: JSON.stringify({ item_id: itemId }),
   })
     .then((response) => {
       if (response.ok) {
-        console.log('Like recorded successfully');
         showSnackbar('Likes recorded successfully!');
       } else {
-        console.error('Error recording likes');
         showSnackbar('Error recording likes!');
       }
     })
     .catch((error) => {
-      console.error('Error recording likes:', error);
-      showSnackbar('Error recording likes!');
+      showSnackbar('Error recording likes!', error);
     });
 };
 
