e478888076a26ba3ed1cf3ce28beac85f2ec3c6d
RileyManda <rileymanda69@gmail.com>
2023-07-06 13:47:18 +0200

    Updated edge case when item is not found

--- src/utils/updateTvShowsCount.js
+++ src/utils/updateTvShowsCount.js
@@ -2,6 +2,8 @@ const updateTvShowsCount = (count) => {
   const countElement = document.getElementById('tvshows-count');
   if (countElement) {
     countElement.textContent = `TvShows ${count}`;
+  } else {
+    console.log('countElement not found');
   }
 };
 
--- src/utils/updateTvShowsCount.test.js
+++ src/utils/updateTvShowsCount.test.js
@@ -2,20 +2,29 @@ import updateTvShowsCount from './updateTvShowsCount.js';
 
 // This test follows the AAA Pattern as required Arrange
 describe('updateTvShowsCount', () => {
+  let countElement;
+
   beforeEach(() => {
-    const countElement = document.createElement('div');
+    countElement = document.createElement('div');
     countElement.setAttribute('id', 'tvshows-count');
     document.body.appendChild(countElement);
   });
 
   afterEach(() => {
-    const countElement = document.getElementById('tvshows-count');
-    document.body.removeChild(countElement);
+    if (countElement && countElement.parentNode) {
+      countElement.parentNode.removeChild(countElement);
+    }
   });
 
   test('should update the TV shows count in the DOM', () => {
     updateTvShowsCount(5);
-    const countElement = document.getElementById('tvshows-count');
     expect(countElement.textContent).toBe('TvShows 5');
+  });
+
+  test('should handle the case when the element is not found', () => {
+    countElement.parentNode.removeChild(countElement);
+    updateTvShowsCount(10);
+    const updatedCountElement = document.getElementById('tvshows-count');
+    expect(updatedCountElement).toBe(null);
   });
 });
