89b2058f96e3bc4932c5b9c8f6e05839dbfcc7ea
RileyManda <rileymanda69@gmail.com>
2023-07-06 14:11:59 +0200

    Refactoring-return error

--- src/utils/updateTvShowsCount.js
+++ src/utils/updateTvShowsCount.js
@@ -1,9 +1,13 @@
 const updateTvShowsCount = (count) => {
-  const countElement = document.getElementById('tvshows-count');
-  if (countElement) {
-    countElement.textContent = `TvShows ${count}`;
-  } else {
-    console.log('countElement not found');
+  try {
+    const countElement = document.getElementById('tvshows-count');
+    if (countElement) {
+      countElement.textContent = `TvShows ${count}`;
+    } else {
+      throw new Error('countElement not found');
+    }
+  } catch (error) {
+    throw new Error('Error fetching likes data', error);
   }
 };
 
